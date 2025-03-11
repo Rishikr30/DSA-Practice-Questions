4c02904e51cd25995c940b518928168bfa541906
Rishikr30 <rishimail3012@gmail.com>
2025-03-11 23:49:25 +0530

    Write a new logic of reverse method

--- Array/reverse_arr.c
+++ Array/reverse_arr.c
@@ -1,18 +1,34 @@
 #include<stdio.h>
-int main()
-{
-    int i,arr[5];
-    printf("Enter 5 diff number :");
+int reverse(){
+    int arr[5];
+    int i,j;
+    printf("Enter 5 diff number :\n");
     for ( i = 0; i <= 4; i++)
     {
         scanf("%d",&arr[i]);
     }
+
+    i = 0;
+    j = 4;
+    while (i<j)
+    {
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]= temp;
+        i++;
+        j--;
+    }
+    
     printf("\nArray in reverse order :");
-    for ( i = 4; i >=0 ; i--)
+    for ( i = 0; i <=4 ; i++)
     {
         printf(" %d",arr[i]);
     }
-    
-    
+     
+}
+
+int main()
+{
+    reverse();
     return 0;
 }
